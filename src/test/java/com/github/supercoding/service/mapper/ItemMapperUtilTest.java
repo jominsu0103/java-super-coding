9682d77dfd6b7428326c1eb775c3bca9f55c8d43
usnimoj <jominsu0103@naver.com>
2024-06-07 15:24:50 +0900

    Test (pure unit testing)

---
+++ src/test/java/com/github/supercoding/service/mapper/ItemMapperUtilTest.java
@@ -0,0 +1,38 @@
+package com.github.supercoding.service.mapper;
+
+import com.github.supercoding.repository.items.ItemEntity;
+import com.github.supercoding.repository.storeSales.StoreSales;
+import com.github.supercoding.web.dto.Item;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Slf4j
+class ItemMapperUtilTest {
+
+    @DisplayName("ItemMapper의 itemEntityToItem 메소드 테스트")
+    @Test
+    void itemEntityToItem() {
+        // given
+        ItemEntity itemEntity = ItemEntity.builder()
+                .name("name")
+                .type("type")
+                .id(1)
+                .price(1000)
+                .stock(0)
+                .cpu("CPU 1")
+                .capacity("5G")
+                .storeSales(new StoreSales())
+                .build();
+        // when
+        Item item = ItemMapper.INSTANCE.itemEntityToItem(itemEntity);
+
+        // then
+        log.info("make item : " +item);
+        assertEquals(itemEntity.getPrice(), item.getPrice());
+        assertEquals(itemEntity.getId().toString(), item.getId());
+        assertEquals(itemEntity.getCpu(), item.getSpec().getCpu());
+        assertEquals(itemEntity.getCapacity(), item.getSpec().getCapacity());
+    }
+}
