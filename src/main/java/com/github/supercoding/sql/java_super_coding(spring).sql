64e614698cb89a847eb723a78997a42f3104421d
usnimoj <jominsu0103@naver.com>
2024-06-24 23:50:00 +0900

    sql JSON_ARRAYAGG update

--- src/main/java/com/github/supercoding/sql/java_super_coding(spring).sql
+++ src/main/java/com/github/supercoding/sql/java_super_coding(spring).sql
@@ -120,3 +120,16 @@ GROUP BY
     o.id, o.total_price, o.order_date;
 
 -- json-arrayagg와 json_object를 사용해 json 형태의 배열로 감싸서 여러 정보들을 취합해 조회할 수 있음
+
+-- @Query(value = "SELECT JSON_ARRAYAGG(" +
+--             "JSON_OBJECT('order_id', o.id, 'total_price', o.total_price, 'order_date', o.order_date, " +
+--             "'items', (SELECT JSON_ARRAYAGG(JSON_OBJECT('item_id', oi.item_id, 'quantity', oi.quantity, 'price_per_unit', oi.price_per_unit, 'name', i.name)) " +
+--             "FROM order_items oi " +
+--             "JOIN items i ON oi.item_id = i.id " +
+--             "WHERE oi.order_id = o.id))) AS order_details " +
+--             "FROM orders o " +
+--             "WHERE o.user_id = ?1 " +
+--             "GROUP BY o.id, o.total_price, o.order_date", nativeQuery = true)
+--     String findOrdersWithItemsByUserId(Integer userId);
+
+-- 배열을 파싱할때 생기는 문제 해결
